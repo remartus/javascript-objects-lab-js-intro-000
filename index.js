bdf468f4a946b2fd8a59bb18c164d4206968ecea
Rose Martus <martus.rose@gmail.com>
2021-03-27 17:44:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = new Object()
 
 //not modifying original, using clone
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.assign({}, {key: value})
+  return Object.assign({}, {key: value})
 }
 
 // we get to destroy
