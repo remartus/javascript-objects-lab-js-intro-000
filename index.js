ec360f8f8a3ce37aabcfc87aa57462548b9d08b8
Rose Martus <martus.rose@gmail.com>
2021-03-27 17:41:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+// define my object
 var recipes = new Object()
+
+//not modifying original, using clone
+function updateObjectWithKeyAndValue(object, key, value){
+  Object.assign({}, {key: value})
+}
